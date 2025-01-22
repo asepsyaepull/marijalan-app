cda7baaee8d855c44a19ef04ba073ab48672504d
asepsyaepull <asep.syaepul95@gmail.com>
2025-01-22 23:37:47 +0700

    update Next.js configuration and add embla-carousel-autoplay dependency

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
   images: {
-    domains: ['travel-journal-api-bootcamp.do.dibimbing.id', 'images.unsplash.com', 's3-alpha-sig.figma.com'],
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'travel-journal-api-bootcamp.do.dibimbing.id',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 's3-alpha-sig.figma.com',
+        hostname: '**',
       },
     ],
   },
