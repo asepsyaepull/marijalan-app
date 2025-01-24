fee0405794359d8a4c961fe310fb75b39065a71a
asepsyaepull <asep.syaepul95@gmail.com>
2025-01-24 13:36:17 +0700

    add Head component to set page title for improved SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@/components/theme-provider";
-import Navbar from "@/components/navbar";
-import Footer from "@/components/footer";
 import { UserProvider } from "@/context/UserContext";
 import { Toaster } from "@/components/ui/toaster";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
       <UserProvider>
-        {pageProps?.pathname !== "/login" && pageProps?.pathname !== "/register" && <Navbar />}
+        <Head>
+          <title>Marijalan</title>
+        </Head>
         <Component {...pageProps} />
-        {pageProps?.pathname == "/login" && pageProps?.pathname == "/register" && <Footer />}
         <Toaster />
       </UserProvider>
     </ThemeProvider>
