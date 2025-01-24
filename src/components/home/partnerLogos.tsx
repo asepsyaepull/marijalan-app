40fe54168b65035d1c2cfbb996a17db1e02f7269
asepsyaepull <asep.syaepul95@gmail.com>
2025-01-24 13:35:50 +0700

    refactor PartnerLogos component to implement marquee for partner logos and adjust layout

--- src/components/home/partnerLogos.tsx
+++ src/components/home/partnerLogos.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image';
+import { cn } from "@/lib/utils";
+import { Marquee } from '../ui/marquee';
 
 const partners = [
     { name: 'Airbnb', logo: '/images/airbnb.svg' },
@@ -9,21 +11,25 @@ const partners = [
     { name: 'Klook', logo: '/images/klook.svg' },
 ];
 
+const firstRow = partners.slice(0, partners.length);
+
 export default function PartnerLogos() {
     return (
-        <section className="py-8 px-4 md:px-10">
+        <section className="py-10 px-4 md:px-8 lg:px-12">
             <div className="container max-w-7xl mx-auto">
-                <div className="flex flex-wrap justify-between items-center md:mx-auto gap-8 md:gap-20 overflow-x-auto scrollbar-hide">
-                    {partners.map((partner) => (
-                        <div key={partner.name} className="w-36 h-10 grayscale hover:grayscale-0 transition-all">
-                            <Image
-                                src={partner.logo}
-                                alt={partner.name}
-                                fill
-                                className="object-contain"
-                            />
-                        </div>
-                    ))}
+                <div className="flex flex-row items-center md:mx-auto gap-8 md:gap-40 overflow-x-auto scrollbar-hide">
+                    <Marquee pauseOnHover className="[--duration:20s] [--gap:40px]">
+                        {firstRow.map((partner) => (
+                            <div key={partner.name} className="w-36 h-10 grayscale hover:grayscale-0 transition-all">
+                                <Image
+                                    src={partner.logo}
+                                    alt={partner.name}
+                                    fill
+                                    className="object-contain"
+                                />
+                            </div>
+                        ))}
+                    </Marquee>
                 </div>
             </div>
         </section>
