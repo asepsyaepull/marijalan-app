e777d38017e6f8afe25bb0e6b946afcc23996d39
asepsyaepull <asep.syaepul95@gmail.com>
2025-01-25 20:57:36 +0700

    add BreadcrumbExperienceDetail component for navigation in experience detail page

---
+++ src/pages/experience/[id]/components/BreadcrumbExperienceDetail.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link"
+
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Home } from "lucide-react"
+
+export function BreadcrumbExperienceDetail() {
+    return (
+        <div className="max-w-7xl px-4 md:mx-auto">
+            <Breadcrumb>
+                <BreadcrumbList>
+                    <BreadcrumbItem>
+                        <BreadcrumbLink>
+                            <Link href="/" className="flex gap-1.5 items-center"><Home className="size-4" />Home</Link>
+                        </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbItem>
+                        <BreadcrumbSeparator />
+                        <BreadcrumbLink>
+                            <Link href="/experience" className="flex gap-1.5 items-center">Experience</Link>
+                        </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                        <BreadcrumbPage>Experience Detail</BreadcrumbPage>
+                    </BreadcrumbItem>
+                </BreadcrumbList>
+            </Breadcrumb>
+        </div>
+    )
+}
