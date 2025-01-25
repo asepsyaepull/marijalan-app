76cb14e040b2d135f033c001f4e883f122ceee5b
asepsyaepull <asep.syaepul95@gmail.com>
2025-01-25 20:57:46 +0700

    refactor BreadcrumbExperience component for improved layout by removing unnecessary padding

--- src/pages/experience/BreadcrumbExperience.tsx
+++ src/pages/experience/BreadcrumbExperience.tsx
@@ -12,7 +12,7 @@ import { Home } from "lucide-react"
 
 export function BreadcrumbExperience() {
     return (
-        <div className="max-w-7xl px-4 py-6 md:mx-auto">
+        <div className="max-w-7xl px-4 md:mx-auto">
             <Breadcrumb>
                 <BreadcrumbList>
                     <BreadcrumbItem>
