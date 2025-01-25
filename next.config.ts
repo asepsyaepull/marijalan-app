be840448c4904f9da1db61a28dc78105e95b4293
asepsyaepull <asep.syaepul95@gmail.com>
2025-01-25 20:58:34 +0700

    add support for Radix UI components and enable strict mode in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,17 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: '**',
+      },
+      {
+        protocol: "https",
+        hostname: "travel-journal-api-bootcamp.do.dibimbing.id", // Sesuaikan dengan domain gambar Anda
+        port: "",
+        pathname: "/**",
       },
     ],
   },
