ecce949071583ef76f360e2fa5811ea7f9294820
asepsyaepull <asep.syaepul95@gmail.com>
2025-01-26 21:19:34 +0700

    refactor: adjust logo component layout and image sizes for better alignment and visibility

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image';
 
 const Logo: FC = () => {
     return (
-        <div className="flex items-center gap-2">
-            <Image src="/iconLogo.svg" alt="Logo" width={24} height={24} />
-            <Image src="/textLogo.svg" alt="Logo" width={32} height={32} className="filter dark:invert" />
+        <div className="flex items-center gap-1">
+            <Image src="/iconLogo.svg" alt="Logo" width={40} height={40} />
+            <Image src="/textLogo.svg" alt="Logo" width={108} height={108} className="filter dark:invert" />
         </div>
     );
 };
