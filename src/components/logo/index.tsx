357c7ba34ce12bff29f8243eea8929386d1eeb7c
asepsyaepull <asep.syaepul95@gmail.com>
2025-01-29 19:03:31 +0700

    feat: add click functionality to Logo component for navigation to home

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -1,13 +1,22 @@
 import { FC } from 'react';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
-const Logo: FC = () => {
+interface LogoProps {
+    className ?: string;
+}
+
+export default function Logo({ className }: LogoProps) {
+    const router = useRouter();
+
+    const handleLogoClick = () => {
+        router.push('/');
+    };
+
     return (
-        <div className="flex items-center gap-1">
+        <div className={`flex items-center gap-1 ${className}`} onClick={handleLogoClick} style={{ cursor: 'pointer' }}>
             <Image src="/iconLogo.svg" alt="Logo" width={40} height={40} />
             <Image src="/textLogo.svg" alt="Logo" width={108} height={108} className="filter dark:invert" />
         </div>
     );
 };
-
-export default Logo;
