c223a880f0a4c044e92e9fc31d1de9d283192ea3
asepsyaepull <asep.syaepul95@gmail.com>
2025-02-07 23:02:25 +0700

    feat: enhance login page layout with responsive design adjustments and improved logo sizes

--- src/components/hero/heroLogin.tsx
+++ src/components/hero/heroLogin.tsx
@@ -5,8 +5,8 @@ export default function HeroLoginSection() {
         <div className="relative hidden lg:flex flex-col w-full bg-black/30 p-8">
             <div className="mt-auto mb-32 space-y-4">
                 <div className="flex items-center gap-2">
-                    <Image src="/iconLogo.svg" alt="Logo" className="h-6" width={24} height={24} />
-                    <Image src="/textLogo.svg" alt="Logo" className="h-8 filter invert" width={32} height={32} />
+                    <Image src="/iconLogo.svg" alt="Logo" className="h-6" width={40} height={40} />
+                    <Image src="/textLogo.svg" alt="Logo" className="h-8 filter invert" width={96} height={96} />
                 </div>
                 <h1 className="text-5xl font-bold text-white leading-tight">
                     Let&apos;s Travel The<br />Beautiful World<br />Together
@@ -14,7 +14,6 @@ export default function HeroLoginSection() {
                 <p className="text-white/80 text-lg max-w-md">
                     We always make our customer happy by providing as many choices as possible
                 </p>
-                <p>Don&apos;t have an account? Sign up</p>
             </div>
         </div>
     );
