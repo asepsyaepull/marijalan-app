09f9c337ce804e83d5a32b3815444c8287a1af97
asepsyaepull <asep.syaepul95@gmail.com>
2025-02-08 12:54:24 +0700

    style: enhance dashboard layout with responsive spacing and improved header design

--- src/components/dashboard/DashboardLayout.tsx
+++ src/components/dashboard/DashboardLayout.tsx
@@ -19,14 +19,14 @@ const DashboardLayout = ({ children }: LayoutProps): JSX.Element => {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1 text-black" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 sm:h-20 sm:gap-4">
+          <div className="flex items-center gap-2 px-4 sm:gap-4 sm:px-6">
+            <SidebarTrigger className="-ml-1 text-black sm:-ml-2" />
+            <Separator orientation="vertical" className="mr-2 h-4 sm:mr-4 sm:h-6" />
             <CustomBreadcrumb items={breadcrumbItems} className="flex" />
           </div>
         </header>
-        <div>
+        <div className="p-4 sm:p-6">
           <main>{children}</main>
         </div>
       </SidebarInset>
